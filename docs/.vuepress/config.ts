b1d9c41217cecec2fe3fd069d8be3b10d4be53f4
SqRoots <lixuan.xyz@gmail.com>
2026-03-28 14:16:00 +0800

    修改 config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,7 +14,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/test/',
   lang: 'zh-CN',
   title: '李宣',
   description: '欢迎访问',
